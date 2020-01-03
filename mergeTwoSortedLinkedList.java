ecf2f68bf057a3d053c4a541df033ab608c2f9e2
Mann <pareshmaniyar1@gmail.com>
2020-01-03 11:00:12 +0530

    Fast merging of sorted linkedList; Need to work on space;80

--- mergeTwoSortedLinkedList.java
+++ mergeTwoSortedLinkedList.java
@@ -41,3 +41,46 @@ class Solution {
         return head;
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        ListNode head;
+        if(l1.val < l2.val){
+            head = new ListNode(l1.val);
+            l1 = l1.next;
+        } else {
+            head = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        ListNode ref = head;
+        while(l1 != null || l2 != null){
+            if(l1 == null){
+                ref.next = l2;
+                return head;
+            }
+            if(l2 == null){
+                ref.next = l1;
+                return head;
+            }
+            ListNode temp = new ListNode(l1.val);
+            if(l1.val < l2.val){
+                l1 = l1.next;
+            } else {
+                temp.val = l2.val;
+                l2 = l2.next;
+            }
+            ref.next = temp;
+            ref = temp;
+        }
+        return head;
+    }
+}
