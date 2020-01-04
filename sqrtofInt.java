54e307549ed166bcd43bb26e173acc7930c64adb
Mann <pareshmaniyar1@gmail.com>
2020-01-04 20:05:05 +0530

    Add complete solution of sqrt; Improve efficiency;90;

--- sqrtofInt.java
+++ sqrtofInt.java
@@ -1,16 +1,32 @@
 class Solution {
     public int mySqrt(int x) {
+        if(x == 1) return 1;
         if(x == 0) return 0;
-        int res = x/2;int left = 0; int right = x;
-        if(res == 0) return 1;
-        while(res * res > x || res > 65536){
-            res = res / 2;
-            System.out.println("res1: " + res + ", res * res:" + res * res);
+        if(x == 2) return 1;
+        if(x == 3) return 1;
+        if(x == 4) return 2;
+        if(x < 9) return 2;
+        int left = 0; int right = x/2; int mid = (left + right)/2;
+        if(mid > 46340) {
+            mid = 46340;
+            right = 46340 * 2;
         }
-        while(res * res < x){
-            res = res + 1;
-            System.out.println("res2: " + res + ", res * res:" + res * res);
+        while(left != right){
+            mid = left/2 + right/2;
+            System.out.print("mid: " + mid + ", mid * mid: " + mid * mid + " ");
+            if(mid * mid == x){
+                return mid;
+            } else if(mid * mid > x || mid * mid < 0){
+                if((mid - 1) * (mid - 1) < x) return (mid - 1);
+                right = right/2 + left/2;
+                // System.out.println("right: " + right + ", left: " + left);
+            } else if(mid * mid < x){
+                System.out.print("mid: " + (mid + 1) * (mid + 1));
+                if((mid + 1) * (mid + 1) > x || (mid + 1) * (mid + 1) < 0) return mid;
+                left = right/2 + left/2;
+                // System.out.println("left: " + left + ", right: " + right);
+            }
         }
-        return res - 1;
+        return (left + right) / 2;
     }
 }
