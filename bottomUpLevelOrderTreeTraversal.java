c2847504a6f5a3ad3c9023fa8049bde70488c3ab
Mann <pareshmaniyar1@gmail.com>
2020-01-06 23:44:17 +0530

    Add closer sol;Need to find the bug;90;

--- bottomUpLevelOrderTreeTraversal.java
+++ bottomUpLevelOrderTreeTraversal.java
@@ -9,29 +9,38 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        return postOrderTraversal(root, 0);
+        return postOrderTraversal(root);
     }
-    List<List<Integer>> postOrderTraversal(TreeNode n, int level){
-        
+    List<List<Integer>> postOrderTraversal(TreeNode n){
         List<List<Integer>> root = new ArrayList<List<Integer>>();
-        if(n.left != null){
-            List<List<Integer>> left = new ArrayList<List<Integer>>();
+        if(n == null) return root;
+        if(n!= null && n.left != null){
+            List<List<Integer>> left = postOrderTraversal(n.left);
             for(List list: left){
-                root.add(left);
+                root.add(list);
             }
         }
         
-        List<List<Integer>> right = new ArrayList<List<Integer>>();
-        if(n.right != null){
-            List<List<Integer>> right = new ArrayList<List<Integer>>();
-            for(List list: right){
-                root.add(right);
+        if(n!= null && n.right != null){
+            List<List<Integer>> right = postOrderTraversal(n.right);
+            int rightSize = right.size() - 1;
+            int rootSize = root.size() - 1;
+            for(int i = 0; i < rightSize - rootSize; i++){
+                root.add(i, right.get(i));
+            }
+            System.out.println("temp2: " + (rightSize - rootSize) + ", rightSize: " + rightSize);
+            for(int i = rightSize - rootSize; i <= rightSize; i++){
+                List<Integer> temp = new ArrayList<Integer>();
+                temp.addAll(root.get(rootSize + rightSize - i));
+                temp.addAll(right.get(i));
+                System.out.println("temp: ");
+                root.add(i, temp);
             }
         }
         
         List<Integer> val = new ArrayList<Integer>();
         val.add(n.val);
-        l1.add(val);
+        root.add(val);
         return root;
     }
 }
