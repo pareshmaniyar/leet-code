b8905f7066f9f9e2153727d15ec939093e18e8c1
Mann <pareshmaniyar1@gmail.com>
2020-01-06 19:59:11 +0530

    Add complete and efficient solution for Checking same tree;85;

--- validPalindrome.java
+++ validPalindrome.java
@@ -3,22 +3,26 @@ class Solution {
         int len = s.length();
         if(len == 0) return true;
         int left = 0; int right = len - 1;
-        while(left != right){
-            while(left < len - 2 && !Character.isLetter(s.charAt(left))){
+        while(left < len && right > 0){
+            while(left < len && !Character.isLetter(s.charAt(left))){
                 left = left + 1;
             }
-            while(right > 0 && !Character.isLetter(s.charAt(right))){
+            while(right > -1 && !Character.isLetter(s.charAt(right))){
                 right = right - 1;
             }
-            System.out.println(left + ": 1");
-            if(left < len && right >= 0 && Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))){
+            // System.out.println(left + ":: " + right);
+            if(left < len && right >= 0){
+            if(Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))){
                 left = left + 1;
                 right = right - 1;
             } else {
-                System.out.println(left + ": ");
-                System.out.println(left + ": " + Character.toLowerCase(s.charAt(left)) + ", "+right+":" + Character.toLowerCase(s.charAt(right)));
-                System.out.println(", len: "+ len + " ," + (Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) );
+                System.out.println(left + ": " + right);
+                // System.out.println(left + ": " + Character.toLowerCase(s.charAt(left)) + ", "+right+":" + Character.toLowerCase(s.charAt(right)));
+                // System.out.println(", len: "+ len + " ," + (Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) );
                 return false;
+            }
+            } else {
+                return true;
             }
         }
         return true;
