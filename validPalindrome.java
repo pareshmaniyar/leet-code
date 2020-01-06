6339a761a279c8ab9aa44ed6a8630af42b9c2e87
Mann <pareshmaniyar1@gmail.com>
2020-01-06 20:53:02 +0530

    Add Efficient sol to valid palindrom string;80;

--- validPalindrome.java
+++ validPalindrome.java
@@ -3,27 +3,19 @@ class Solution {
         int len = s.length();
         if(len == 0) return true;
         int left = 0; int right = len - 1;
-        while(left < len && right >= 0){
-            while(left < len - 1 && !Character.isLetterOrDigit(s.charAt(left))){
+        while(left <= right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
                 left = left + 1;
             }
-            while(right > -1 && !Character.isLetterOrDigit(s.charAt(right))){
+            while(right > left && !Character.isLetterOrDigit(s.charAt(right))){
                 right = right - 1;
             }
-            // System.out.println(left + ":: " + right);
-            if(left < len && right >= 0){
-                if(Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))){
-                    left = left + 1;
-                    right = right - 1;
-                } else {
-                    // System.out.println(left + ": " + right);
-                    // System.out.println(left + ": " + Character.toLowerCase(s.charAt(left)) + ", "+right+":" + Character.toLowerCase(s.charAt(right)));
-                    // System.out.println(", len: "+ len + " ," + (Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) );
-                    return false;
-                }
+            if(left < right && Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))){
+                left = left + 1;
+                right = right - 1;
             } else {
-                // System.out.println(left + ": " + right);
-                return true;
+                if(left >= right) return true;
+                return false;
             }
         }
         return true;
